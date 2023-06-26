5a8de7bb9801f8bd03fc10433e9e9c4a66080b69
Kalana Dananjaya <kalanadananjaya855@gmail.com>
2023-06-27 00:28:16 +0530

    finished 95%

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+/*
 let circularProgress = document.querySelector(".progress").
     progressValue = document.querySelector(".progress-value");
 
@@ -9,11 +10,20 @@ let progress = setInterval(() =>{
     progressStartValue++;
     progressValue.textContent = ${progressStartValue}%
 
-   /* progressValue.textContent = ${progressStartValue}%
-*/
+   /!* progressValue.textContent = ${progressStartValue}%
+*!/
         if (progressStartValue == progressEndValue){
             clearInterval(progress);
         }
     console.log(progressStartValue);
 
-},speed);
+},speed);*/
+
+var typed = new Typed(".multiple-text",{
+    strings: ["Full Stack Developer","Web Developer","Graphic Desinger","Kalana"],
+    typeSpeed:100,
+    backSpeed :100,
+    backDelay : 1000,
+    loop:true
+})
+
